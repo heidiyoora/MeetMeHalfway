2bf76190fff2f14399029ec4db5b178019e3dafc
Heidi Kim <heidiyoorakim@gmail.com>
2021-01-13 18:18:31 -0500

    successful opencage fetch requests

--- client/components/AddressInput.jsx
+++ client/components/AddressInput.jsx
@@ -9,15 +9,19 @@ const AddressInput = (props) => {
       <input type='text' id='friendCity' placeholder='City' />
       <br></br>
       <input type='text' id='friendState' placeholder='State' />
+      <input type='text' id='friendZipcode' placeholder='Zipcode' />
       <br></br>
 
       <button className='findFood' type='button' onClick={() => {
         const friendAddress = document.getElementById('friendAddress').value;
         const friendCity = document.getElementById('friendCity').value;
         const friendState = document.getElementById('friendState').value;
+        const friendZipcode = document.getElementById('friendZipcode').value;
 
-        const fullAddress = friendAddress + ' ' + friendCity + ' ' + friendState;
-        //console.log(fullAddress);
+        if (!friendAddress || !friendCity || !friendState || !friendZipcode) return alert('all fields are required');
+
+        let fullAddress = friendAddress + ' ' + friendCity + ' ' + friendState + ' ' + friendZipcode;
+
         props.grubButton(fullAddress);
       }} > Let's get grubbing! </button>
     </div>
--- server/controllers/locationController.js
+++ server/controllers/locationController.js
@@ -5,9 +5,25 @@ const locationController = {};
 const openCageAPIKey = '917441060cc446ec958162b37b44dc9c';
 
 locationController.forwardGeocode = (req, res, next) => {
-  console.log('locationCont.forwardGeocode: ', req.body.address);
-  // fetch OpenCage API request
+  // verify requested address is a string
+  if (typeof req.body.address !== 'string'){
+    return next({
+      log: 'ERROR: locationController.forwardGeocode - requested address is not a string',
+      message: { err: 'Internal Server Error'}
+    });
+  }
+  // sanitize string to have no white spaces
+  const address = req.body.address.replace(/\s/g, '%20');
+  console.log('locationCont.forwardGeocode: ', address);
 
+  
+  // fetch OpenCage API request to retrieve coords
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${openCageAPIKey}&bounds=-76.12021,39.55986,-71.68899,41.81065&countrycode=us&limit=1`)
+    .then((response) => response.json())
+    .then((data) => {console.log(data.results)
+    
+    })
+    .catch((err) => console.log('error in Open Cage Fetch request: ', err))
 
   return next();
 };
